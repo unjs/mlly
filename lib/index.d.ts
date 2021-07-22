b4ccb3f2667f6d4022a24d41dbaaa93514a3e5ac
Pooya Parsa <pyapar@gmail.com>
2021-07-22 16:14:52 +0200

    chore: fix type exports

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,7 +4,8 @@ export interface CommonjsContext {
   __filename: string
   __dirname: string
 }
-export type createCommonJS = (importMeta: ImportMeta) => CommonjsContext
+
+export function createCommonJS (importMeta: ImportMeta) : CommonjsContext
 
 // Resolve
 
@@ -13,23 +14,23 @@ export interface ResolveOptions {
   conditions?: string[]
 }
 
-export type resolve = (id: string, opts: ResolveOptions) => Promise<string>
-export type resolvePath = (id: string, opts: ResolveOptions) => Promise<string>
-export type resolveSync = (id: string, opts: ResolveOptions) => string
-export type resolvePathSync = (id: string, opts: ResolveOptions) => string
-export type createResolve = (defaults: ResolveOptions) => (id: string, from: string | URL) => Promise<string>
-export type resolveImports = (code: string, opts: ResolveOptions) => Promise<string>
+export function resolve (id: string, opts: ResolveOptions) : Promise<string>
+export function resolvePath (id: string, opts: ResolveOptions) : Promise<string>
+export function resolveSync (id: string, opts: ResolveOptions) : string
+export function resolvePathSync (id: string, opts: ResolveOptions) : string
+export function createResolve (defaults: ResolveOptions) : (id: string, from: string | URL) => Promise<string>
+export function resolveImports (code: string, opts: ResolveOptions) : Promise<string>
 
 
 // Evaluate
 
 export interface EvaluateOptions extends ResolveOptions {}
+export function loadModule (id: string, opts?: EvaluateOptions) : Promise<any>
+export function evalModule (code: string, opts?: EvaluateOptions) : Promise<any>
 
-export type loadModule = (id: string, opts?: EvaluateOptions) => Promise<any>
-export type evalModule = (code: string, opts?: EvaluateOptions) => Promise<any>
-export type readModule = (id: string, opts?: ResolveOptions) => Promise<any>
-export type toDataURL = (code: string) => Promise<string>
+export function readModule (id: string, opts?: ResolveOptions) : Promise<any>
+export function toDataURL(code: string, opts?: ResolveOptions) : Promise<string>
 
 // Path Utils
 
-export type fileURLToPath = (id: URL | string) => string
+export function fileURLToPath (id: URL | string) : string
