815bf5545b9f73cd8d8b3b7a651dfb7c26de6f24
Pooya Parsa <pyapar@gmail.com>
2021-10-01 00:09:17 +0200

    chore: update fixture imports

--- test/fixture/cjs.mjs
+++ test/fixture/cjs.mjs
@@ -3,4 +3,4 @@ import { createCommonJS } from 'mlly'
 const cjs = createCommonJS(import.meta.url)
 
 console.log(cjs)
-console.log(cjs.require.resolve('../package.json'))
+console.log(cjs.require.resolve('../../package.json'))
--- test/fixture/eval.mjs
+++ test/fixture/eval.mjs
@@ -11,6 +11,6 @@ await evalModule(`
 
 await loadModule('./hello.mjs', { url: import.meta.url })
 
-console.log(await loadModule('../package.json', { url: import.meta.url }).then(r => r.default.name))
+console.log(await loadModule('../../package.json', { url: import.meta.url }).then(r => r.default.name))
 
 await loadModule('./eval-err.mjs', { url: import.meta.url }).catch(e => console.error(e))
