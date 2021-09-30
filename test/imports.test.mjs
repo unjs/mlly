cfbe1a578c806de98169fde7932c58888664d89c
Daniel Roe <daniel@roe.dev>
2021-09-30 13:33:15 +0100

    refactor: split out `parseImport` and `matchImports` utilities

--- test/imports.test.mjs
+++ test/imports.test.mjs
@@ -4,91 +4,69 @@ import { matchESMImports } from '../lib/index.mjs'
 const tests = {
   'import defaultMember from "module-name";': {
     from: 'module-name',
-    imports: {
-      defaultImport: 'defaultMember',
-      namespacedImport: undefined,
-      namedImports: {}
-    }
+    defaultImport: 'defaultMember',
+    namespacedImport: undefined,
+    namedImports: {}
   },
   'import *    as name from "module-name  ";': {
     from: 'module-name',
-    imports: {
-      defaultImport: undefined,
-      namespacedImport: 'name',
-      namedImports: {}
-    }
+    defaultImport: undefined,
+    namespacedImport: 'name',
+    namedImports: {}
   },
   'import *    as name from "module-name  "; //test': {
     from: 'module-name',
-    imports: {
-      defaultImport: undefined,
-      namespacedImport: 'name',
-      namedImports: {}
-    }
+    defaultImport: undefined,
+    namespacedImport: 'name',
+    namedImports: {}
   },
   'import { member } from "  module-name";': {
     from: 'module-name',
-    imports: {
-      defaultImport: undefined,
-      namespacedImport: undefined,
-      namedImports: { member: 'member' }
-    }
+    defaultImport: undefined,
+    namespacedImport: undefined,
+    namedImports: { member: 'member' }
   },
   'import { member as alias } from "module-name";': {
     from: 'module-name',
-    imports: {
-      defaultImport: undefined,
-      namespacedImport: undefined,
-      namedImports: { member: 'alias' }
-    }
+    defaultImport: undefined,
+    namespacedImport: undefined,
+    namedImports: { member: 'alias' }
   },
   'import { member1, member2 as alias2, member3 as alias3 } from "module-name";': {
     from: 'module-name',
-    imports: {
-      defaultImport: undefined,
-      namespacedImport: undefined,
-      namedImports: { member1: 'member1', member2: 'alias2', member3: 'alias3' }
-    }
+    defaultImport: undefined,
+    namespacedImport: undefined,
+    namedImports: { member1: 'member1', member2: 'alias2', member3: 'alias3' }
   },
   'import { member1, /* member0point5, */ member2 as alias2, member3 as alias3 } from "module-name";': {
     from: 'module-name',
-    imports: {
-      defaultImport: undefined,
-      namespacedImport: undefined,
-      namedImports: { member1: 'member1', member2: 'alias2', member3: 'alias3' }
-    }
+    defaultImport: undefined,
+    namespacedImport: undefined,
+    namedImports: { member1: 'member1', member2: 'alias2', member3: 'alias3' }
   },
   'import defaultMember, { member, /* test */ member } from "module-name";': {
     from: 'module-name',
-    imports: {
-      defaultImport: 'defaultMember',
-      namespacedImport: undefined,
-      namedImports: { member: 'member' }
-    }
+    defaultImport: 'defaultMember',
+    namespacedImport: undefined,
+    namedImports: { member: 'member' }
   },
   'import defaultMember, * as name from "module-name";': {
     from: 'module-name',
-    imports: {
-      defaultImport: 'defaultMember',
-      namespacedImport: 'name',
-      namedImports: {}
-    }
+    defaultImport: 'defaultMember',
+    namespacedImport: 'name',
+    namedImports: {}
   },
   'import "module-name";': {
     from: 'module-name',
-    imports: {
-      defaultImport: undefined,
-      namespacedImport: undefined,
-      namedImports: {}
-    }
+    defaultImport: undefined,
+    namespacedImport: undefined,
+    namedImports: {}
   },
   'import Foo, { type Baz, bar, type Type as RenamedType } from "module-name";': {
     from: 'module-name',
-    imports: {
-      defaultImport: 'Foo',
-      namespacedImport: undefined,
-      namedImports: { bar: 'bar' }
-    }
+    defaultImport: 'Foo',
+    namespacedImport: undefined,
+    namedImports: { bar: 'bar' }
   }
 }
 
@@ -98,11 +76,9 @@ tests[`import {
   member2
 } from "module-name";`] = {
   from: 'module-name',
-  imports: {
-    defaultImport: undefined,
-    namespacedImport: undefined,
-    namedImports: { member1: 'member1', member2: 'member2' }
-  }
+  defaultImport: undefined,
+  namespacedImport: undefined,
+  namedImports: { member1: 'member1', member2: 'member2' }
 }
 
 tests[`import {
@@ -111,22 +87,18 @@ tests[`import {
   member2
 } from "module-name";`] = {
   from: 'module-name',
-  imports: {
-    defaultImport: undefined,
-    namespacedImport: undefined,
-    namedImports: { member1: 'member1', member2: 'member2' }
-  }
+  defaultImport: undefined,
+  namespacedImport: undefined,
+  namedImports: { member1: 'member1', member2: 'member2' }
 }
 
 tests[`import {
   Component
 } from '@angular2/core';`] = {
   from: '@angular2/core',
-  imports: {
-    defaultImport: undefined,
-    namespacedImport: undefined,
-    namedImports: { Component: 'Component' }
-  }
+  defaultImport: undefined,
+  namespacedImport: undefined,
+  namedImports: { Component: 'Component' }
 }
 
 tests[`
@@ -134,20 +106,20 @@ tests[`
   import Foo, type { Type } from "module-name";
 `] = {
   from: 'module-name',
-  imports: {
-    defaultImport: 'Foo',
-    namespacedImport: undefined,
-    namedImports: {}
-  }
+  defaultImport: 'Foo',
+  namespacedImport: undefined,
+  namedImports: {}
 }
 
 describe('matchESMImports', () => {
   for (const test in tests) {
     it(test.replace(/\n/g, '\\n'), () => {
-      const matched = matchESMImports(test)
+      const matched = matchESMImports(test).filter(details => !details.isType)
       expect(matched.length).to.equal(1)
-      expect(matched[0].from).to.equal(tests[test].from)
-      expect(matched[0].imports).to.deep.equal(tests[test].imports)
+      delete matched[0].code
+      delete matched[0].location
+      delete matched[0].imports
+      expect(matched[0]).to.deep.equal(tests[test])
     })
   }
 })
