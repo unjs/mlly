e4c05e727f082c294d177902ddb6035d2a274207
Pooya Parsa <pyapar@gmail.com>
2021-09-30 23:45:28 +0200

    refactor: simplify test

--- test/imports.test.mjs
+++ test/imports.test.mjs
@@ -7,73 +7,44 @@ const staticTests = {
 
   'import defaultMember from "module-name";': {
     specifier: 'module-name',
-    type: 'static',
-    defaultImport: 'defaultMember',
-    namespacedImport: undefined,
-    namedImports: {}
+    defaultImport: 'defaultMember'
   },
   'import *    as name from "module-name  ";': {
     specifier: 'module-name',
-    type: 'static',
-    defaultImport: undefined,
-    namespacedImport: 'name',
-    namedImports: {}
+    namespacedImport: 'name'
   },
   'import *    as name from "module-name  "; //test': {
     specifier: 'module-name',
-    type: 'static',
-    defaultImport: undefined,
-    namespacedImport: 'name',
-    namedImports: {}
+    namespacedImport: 'name'
   },
   'import { member } from "  module-name";': {
     specifier: 'module-name',
-    type: 'static',
-    defaultImport: undefined,
-    namespacedImport: undefined,
     namedImports: { member: 'member' }
   },
   'import { member as alias } from "module-name";': {
     specifier: 'module-name',
-    type: 'static',
-    defaultImport: undefined,
-    namespacedImport: undefined,
     namedImports: { member: 'alias' }
   },
   'import { member1, member2 as alias2, member3 as alias3 } from "module-name";': {
     specifier: 'module-name',
-    type: 'static',
-    defaultImport: undefined,
-    namespacedImport: undefined,
     namedImports: { member1: 'member1', member2: 'alias2', member3: 'alias3' }
   },
   'import { member1, /* member0point5, */ member2 as alias2, member3 as alias3 } from "module-name";': {
     specifier: 'module-name',
-    type: 'static',
-    defaultImport: undefined,
-    namespacedImport: undefined,
     namedImports: { member1: 'member1', member2: 'alias2', member3: 'alias3' }
   },
   'import defaultMember, { member, /* test */ member } from "module-name";': {
     specifier: 'module-name',
-    type: 'static',
     defaultImport: 'defaultMember',
-    namespacedImport: undefined,
     namedImports: { member: 'member' }
   },
   'import defaultMember, * as name from "module-name";': {
     specifier: 'module-name',
-    type: 'static',
     defaultImport: 'defaultMember',
-    namespacedImport: 'name',
-    namedImports: {}
+    namespacedImport: 'name'
   },
   'import "module-name";': {
-    specifier: 'module-name',
-    type: 'static',
-    defaultImport: undefined,
-    namespacedImport: undefined,
-    namedImports: {}
+    specifier: 'module-name'
   }
 }
 
@@ -83,9 +54,6 @@ staticTests[`import {
   member2
 } from "module-name";`] = {
   specifier: 'module-name',
-  type: 'static',
-  defaultImport: undefined,
-  namespacedImport: undefined,
   namedImports: { member1: 'member1', member2: 'member2' }
 }
 
@@ -95,9 +63,6 @@ staticTests[`import {
   member2
 } from "module-name";`] = {
   specifier: 'module-name',
-  type: 'static',
-  defaultImport: undefined,
-  namespacedImport: undefined,
   namedImports: { member1: 'member1', member2: 'member2' }
 }
 
@@ -106,29 +71,23 @@ staticTests[`import {
 } from '@angular2/core';`] = {
   specifier: '@angular2/core',
   type: 'static',
-  defaultImport: undefined,
-  namespacedImport: undefined,
   namedImports: { Component: 'Component' }
 }
 
 // -- Dynamic import --
 const dynamicTests = {
   'const { test, /* here */, another, } = await import ( "module-name" );': {
-    expression: '"module-name"',
-    type: 'dynamic',
-    defaultImport: undefined,
-    namespacedImport: undefined,
-    namedImports: {
-      test: 'test',
-      another: 'another'
-    }
+    expression: '"module-name"'
   },
   'var promise = import ( "module-name" );': {
     expression: '"module-name"',
     type: 'dynamic',
-    defaultImport: 'promise',
-    namespacedImport: undefined,
-    namedImports: {}
+    defaultImport: 'promise'
+  },
+  'import ( "module-name" );': {
+    expression: '"module-name"',
+    type: 'dynamic',
+    defaultImport: 'promise'
   }
 }
 
@@ -144,9 +103,15 @@ describe('findStaticImports', () => {
       expect(match.specifier).to.equal(test.specifier)
 
       const parsed = parseStaticImport(match)
-      expect(parsed.defaultImport).to.equals(test.defaultImport)
-      expect(parsed.namedImports).to.eql(test.namedImports)
-      expect(parsed.namespacedImport).to.eql(test.namespacedImport)
+      if (test.defaultImport) {
+        expect(parsed.defaultImport).to.equals(test.defaultImport)
+      }
+      if (test.namedImports) {
+        expect(parsed.namedImports).to.eql(test.namedImports)
+      }
+      if (test.namespacedImport) {
+        expect(parsed.namespacedImport).to.eql(test.namespacedImport)
+      }
     })
   }
 })
