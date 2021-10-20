50f13b1a2cb4c191d1546f224b4315ed8948ed78
Pooya Parsa <pyapar@gmail.com>
2021-10-20 19:37:19 +0200

    fix(pkg): inline `import-meta-resolve`
    fixing esm issue with `cjs` target

--- build.config.ts
+++ build.config.ts
@@ -3,6 +3,7 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   declaration: true,
   emitCJS: true,
+  inlineDependencies: true,
   entries: [
     'src/index'
   ]
