97d4da16d6b08b0e251084fa2d4e0adefc81f5b4
Daniel Roe <daniel@roe.dev>
2021-10-22 21:01:05 +0100

    fix: cover more cases in regexps

--- src/syntax.ts
+++ src/syntax.ts
@@ -1,9 +1,10 @@
-const ESM_RE = /([\s;]|^)(import[ '"{]|export\b)/m
+const ESM_RE = /([\s;]|^)(import[\s'"*{]|export\b|import\.meta\b)/m
+
 export function hasESMSyntax (code: string): boolean {
   return ESM_RE.test(code)
 }
 
-const CJS_RE = /([\s;]|^)(module.exports\b|exports\.)/m
+const CJS_RE = /([\s;]|^)(module.exports\b|exports\.|require\s*\(|global\b)/m
 export function hasCJSSyntax (code: string): boolean {
   return CJS_RE.test(code)
 }
@@ -12,9 +13,15 @@ export function detectSyntax (code: string): 'esm' | 'cjs' | 'mixed' | 'unknown'
   const isESM = hasESMSyntax(code)
   const isCJS = hasCJSSyntax(code)
 
-  if (isESM && isCJS) return 'mixed'
-  if (isESM) return 'esm'
-  if (isCJS) return 'cjs'
+  if (isESM && isCJS) {
+    return 'mixed'
+  }
+  if (isESM) {
+    return 'esm'
+  }
+  if (isCJS) {
+    return 'cjs'
+  }
 
   return 'unknown'
 }
--- test/syntax.test.mjs
+++ test/syntax.test.mjs
@@ -2,13 +2,27 @@ import { expect } from 'chai'
 import { detectSyntax } from 'mlly'
 
 const staticTests = {
+  // ESM
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import#syntax
+  'import defaultExport from "module-name";': 'esm',
+  'import * as name from "module-name";': 'esm',
+  'import { export1 } from "module-name";': 'esm',
+  'import { export1 as alias1 } from "module-name";': 'esm',
+  'import { export1, export2 } from "module-name";': 'esm',
+  'import { export1, export2 as alias2, export3 } from "module-name";': 'esm',
+  'import defaultExport, { export1, export2 } from "module-name";': 'esm',
+  'import defaultExport, * as name from"module-name";': 'esm',
+  'import"module-name"': 'esm',
+  //
   'import defaultMember from "module-name";': 'esm',
-  'import"module-name  ";': 'esm',
-  'export default b=""': 'esm',
   'import "./file.mjs"': 'esm',
+  'export default b=""': 'esm',
+  // CJS
   'exports.c={}': 'cjs',
   'const b=true;module.exports={b};': 'cjs',
+  // Mixed
   'import"module-name";module.exports={};': 'mixed',
+  // No clues
   'import("./file.mjs")': 'unknown',
   'console.log(process.version)': 'unknown',
   'const a={};': 'unknown'
