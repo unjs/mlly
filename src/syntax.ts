32ef24a86371b50019c07417e90e05aef5d4bb44
Daniel Roe <daniel@roe.dev>
2021-10-27 14:55:54 +0100

    feat: add `isValidNodeImport` utility (#16)

--- src/syntax.ts
+++ src/syntax.ts
@@ -1,4 +1,11 @@
+import { promises as fsp } from 'fs'
+import { extname } from 'pathe'
+import { readPackageJSON } from 'pkg-types'
+import { ResolveOptions, resolvePath } from './resolve'
+
 const ESM_RE = /([\s;]|^)(import[\s'"*{]|export\b|import\.meta\b)/m
+
+const BUILTIN_EXTENSIONS = new Set(['.mjs', '.cjs', '.node', '.wasm'])
 
 export function hasESMSyntax (code: string): boolean {
   return ESM_RE.test(code)
@@ -18,4 +25,28 @@ export function detectSyntax (code: string) {
     hasCJS,
     isMixed: hasESM && hasCJS
   }
+}
+
+export async function isValidNodeImport (id: string, opts: ResolveOptions & { code?: string } = {}): Promise<boolean> {
+  const resolvedPath = await resolvePath(id, opts)
+  const extension = extname(resolvedPath)
+
+  if (BUILTIN_EXTENSIONS.has(extension)) {
+    return true
+  }
+
+  if (extension !== '.js') {
+    return false
+  }
+
+  if (resolvedPath.match(/\.(\w+-)?esm?(-\w+)?\.js$/)) {
+    return false
+  }
+
+  const pkg = await readPackageJSON(resolvedPath).catch(() => null)
+  if (pkg?.type === 'module') { return true }
+
+  const code = opts.code || await fsp.readFile(resolvedPath, 'utf-8').catch(() => null)
+
+  return !hasESMSyntax(code)
 }
