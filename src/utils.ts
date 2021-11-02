ced0a9cf5382408aae771c2d603eb33d9eeac46c
Daniel Roe <daniel@roe.dev>
2021-11-02 16:25:14 +0000

    feat: add `sanitizeFilename` helper

--- src/utils.ts
+++ src/utils.ts
@@ -9,6 +9,20 @@ export function fileURLToPath (id: string): string {
   return normalizeSlash(_fileURLToPath(id))
 }
 
+// https://datatracker.ietf.org/doc/html/rfc2396
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_RE = /[?*:\x00-\x1F\x7F<>#"{}|^[\]`]/g
+
+// See https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+export function sanitizeFilename (name: string = ''): string {
+  const match = /^[a-z]:/i.exec(name)
+  const driveLetter = match ? match[0] : ''
+
+  // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+  // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+  return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_RE, '_')
+}
+
 export function normalizeid (id: string): string {
   if (typeof id !== 'string') {
     // @ts-ignore
--- test/utils.test.mjs
+++ test/utils.test.mjs
@@ -1,4 +1,4 @@
-import { isNodeBuiltin } from 'mlly'
+import { isNodeBuiltin, sanitizeFilename } from 'mlly'
 import { expect } from 'chai'
 
 describe('isNodeBuiltin', () => {
@@ -19,5 +19,25 @@ describe('isNodeBuiltin', () => {
 
   it('undefined', () => {
     expect(isNodeBuiltin()).to.equal(false)
+  })
+})
+
+describe('sanitizeFilename', () => {
+  const cases = {
+    // [?*:\x00-\x1F\x7F<>#"{}|^[\]`]
+    'C:/te#st/[...slug].jsx': 'C:/te_st/_...slug_.jsx',
+    'C:\\te#st\\[...slug].jsx': 'C:\\te_st\\_...slug_.jsx',
+    '/te#st/[...slug].jsx': '/te_st/_...slug_.jsx',
+    '\0a?b*c:d\x7Fe<f>g#h"i{j}k|l^m[n]o`p.jsx': '_a_b_c_d_e_f_g_h_i_j_k_l_m_n_o_p.jsx',
+    '': ''
+  }
+  for (const id in cases) {
+    it(`'${id}': ${cases[id]}`, () => {
+      expect(sanitizeFilename(id)).to.equal(cases[id])
+    })
+  }
+
+  it('undefined', () => {
+    expect(sanitizeFilename()).to.equal('')
   })
 })
