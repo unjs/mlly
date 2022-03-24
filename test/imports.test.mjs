bc64246a907b09c597093b36da93291a23cb305b
Daniel Roe <daniel@roe.dev>
2022-03-24 15:51:46 +0000

    fix: improve regexp for multiple imports on same line (#41)

--- test/imports.test.mjs
+++ test/imports.test.mjs
@@ -45,7 +45,11 @@ const staticTests = {
   },
   'import "module-name";': {
     specifier: 'module-name'
-  }
+  },
+  'import { thing } from "module-name";import { other } from "other-module"': [{
+    specifier: 'module-name',
+    namedImports: { thing: 'thing' }
+  }]
 }
 
 staticTests[`import {
@@ -94,25 +98,27 @@ const dynamicTests = {
 }
 
 describe('findStaticImports', () => {
-  for (const [input, test] of Object.entries(staticTests)) {
+  for (const [input, _results] of Object.entries(staticTests)) {
     it(input.replace(/\n/g, '\\n'), () => {
       const matches = findStaticImports(input)
-      expect(matches.length).to.equal(1)
+      const results = Array.isArray(_results) ? _results : [_results]
+      for (let i = 0; i < results.length; i++) {
+        const test = results[i]
+        const match = matches[i]
+        expect(match.type).to.equal('static')
 
-      const match = matches[0]
-      expect(match.type).to.equal('static')
+        expect(match.specifier).to.equal(test.specifier)
 
-      expect(match.specifier).to.equal(test.specifier)
-
-      const parsed = parseStaticImport(match)
-      if (test.defaultImport) {
-        expect(parsed.defaultImport).to.equals(test.defaultImport)
-      }
-      if (test.namedImports) {
-        expect(parsed.namedImports).to.eql(test.namedImports)
-      }
-      if (test.namespacedImport) {
-        expect(parsed.namespacedImport).to.eql(test.namespacedImport)
+        const parsed = parseStaticImport(match)
+        if (test.defaultImport) {
+          expect(parsed.defaultImport).to.equals(test.defaultImport)
+        }
+        if (test.namedImports) {
+          expect(parsed.namedImports).to.eql(test.namedImports)
+        }
+        if (test.namespacedImport) {
+          expect(parsed.namespacedImport).to.eql(test.namespacedImport)
+        }
       }
     })
   }
