7c1bda4221b8a162e39610ebf9f50f957a32d7f9
Rafał Chłodnicki <rchl2k@gmail.com>
2022-06-16 14:43:54 +0200

    fix: make `url` optional in resolver created with `createResolve` (#44)

--- test/fixture/resolve.mjs
+++ test/fixture/resolve.mjs
@@ -1,9 +1,9 @@
 import { resolvePath, createResolve, resolveImports } from 'mlly'
 
-import.meta.resolve = createResolve({ url: import.meta.url })
-console.log(await import.meta.resolve('./cjs.mjs'))
+const resolve = createResolve({ url: import.meta.url })
+console.log(await resolve('./cjs.mjs'))
 
 console.log(await resolvePath('./cjs.mjs', { url: import.meta.url }))
 console.log(await resolvePath('./foo', { url: import.meta.url }))
 
-console.log(await resolveImports(`import foo from './eval.mjs'`, { url: import.meta.url }))
+console.log(await resolveImports('import foo from \'./eval.mjs\'', { url: import.meta.url }))
