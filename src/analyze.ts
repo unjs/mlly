2f9f99cb4be42d58ad4e2e303c0b5df02719168d
Pooya Parsa <pooya@pi0.io>
2024-01-11 12:39:33 +0100

    minify regex

--- src/analyze.ts
+++ src/analyze.ts
@@ -81,7 +81,7 @@ const EXPORT_NAMED_DESTRUCT =
 const EXPORT_STAR_RE =
   /\bexport\s*(\*)(\s*as\s+(?<name>[\w$]+)\s+)?\s*(\s*from\s*["']\s*(?<specifier>(?<="\s*)[^"]*[^\s"](?=\s*")|(?<='\s*)[^']*[^\s'](?=\s*'))\s*["'][^\n;]*)?/g;
 const EXPORT_DEFAULT_RE =
-  /\bexport\s+default\s+(async function|function|class|true|false|(\W\D)|\d)|\bexport\s+default\s+(?!async function|function|class|true|false|\W|\d)(?<defaultName>\w+)/g;
+  /\bexport\s+default\s+(async function|function|class|true|false|\W|\d)|\bexport\s+default\s+(?<defaultName>.*)/g;
 const TYPE_RE = /^\s*?type\s/;
 
 export function findStaticImports(code: string): StaticImport[] {
