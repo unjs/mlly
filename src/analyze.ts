52137173a528afa1b80a9425918c2ee23790765a
Máté Nagy <nagy.mate.0125@gmail.com>
2024-01-11 12:40:30 +0100

    feat(findExports): extract name of default exports (#179)
    Co-authored-by: Pooya Parsa <pooya@pi0.io>

--- src/analyze.ts
+++ src/analyze.ts
@@ -80,7 +80,8 @@ const EXPORT_NAMED_DESTRUCT =
   /\bexport\s+(let|var|const)\s+(?:{(?<exports1>[^}]+?)[\s,]*}|\[(?<exports2>[^\]]+?)[\s,]*])\s+=/gm;
 const EXPORT_STAR_RE =
   /\bexport\s*(\*)(\s*as\s+(?<name>[\w$]+)\s+)?\s*(\s*from\s*["']\s*(?<specifier>(?<="\s*)[^"]*[^\s"](?=\s*")|(?<='\s*)[^']*[^\s'](?=\s*'))\s*["'][^\n;]*)?/g;
-const EXPORT_DEFAULT_RE = /\bexport\s+default\s+/g;
+const EXPORT_DEFAULT_RE =
+  /\bexport\s+default\s+(async function|function|class|true|false|\W|\d)|\bexport\s+default\s+(?<defaultName>.*)/g;
 const TYPE_RE = /^\s*?type\s/;
 
 export function findStaticImports(code: string): StaticImport[] {
