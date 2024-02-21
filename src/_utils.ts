4469e7b833874a369dffb5ae882eb6bf0125a3b0
Pooya Parsa <pooya@pi0.io>
2024-02-21 14:06:30 +0100

    perf(resolver): remove intermediate `pcall` util (#230)

--- src/_utils.ts
+++ src/_utils.ts
@@ -6,26 +6,6 @@ export function normalizeSlash(string_) {
   return string_.replace(/\\/g, "/");
 }
 
-export function pcall<TFn extends (...args: any[]) => any>(
-  function_: TFn,
-  ...arguments_: Parameters<TFn>
-): Promise<ReturnType<TFn>> {
-  try {
-    return Promise.resolve(function_(...arguments_)).catch((error) =>
-      perr(error),
-    );
-  } catch (error) {
-    return perr(error);
-  }
-}
-
-export function perr(_error) {
-  const error = new Error(_error);
-  error.code = _error.code;
-  Error.captureStackTrace(error, pcall);
-  return Promise.reject(error);
-}
-
 export function isObject(value) {
   return value !== null && typeof value === "object";
 }
