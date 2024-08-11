6ab8ca4bb230fe5a33f9945d433f9074a5d6faf1
Tobias Diez <code@tobiasdiez.com>
2024-08-11 23:18:56 +0000

    fix: mark `names` field in `ESMExport` as possibly undefined

--- src/_utils.ts
+++ src/_utils.ts
@@ -1,5 +1,9 @@
 import { builtinModules } from "node:module";
 
+// Regular expression with named groups
+export type RegExpWithGroups<T extends string> = RegExp & { __groups: T };
+type ExtractGroupNames<T> = T extends RegExpWithGroups<infer S> ? { [name in S]: string } : never;
+
 export const BUILTIN_MODULES = new Set(builtinModules);
 
 export function normalizeSlash(path: string): string {
@@ -10,12 +14,12 @@ export function isObject(value: unknown): boolean {
   return value !== null && typeof value === "object";
 }
 
-export function matchAll(regex: RegExp, string: string, addition: any) {
+export function matchAll<S extends string, T extends object>(regex: RegExpWithGroups<S>, string: string, addition: T) {
   const matches = [];
   for (const match of string.matchAll(regex)) {
     matches.push({
       ...addition,
-      ...match.groups,
+      ...match.groups as ExtractGroupNames<typeof regex>,
       code: match[0],
       start: match.index,
       end: (match.index || 0) + match[0].length,
